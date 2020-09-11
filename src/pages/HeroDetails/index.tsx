6552ba4d5aa159075aa545ca08b152bdec618aa5
Gean Lucas <gean.lfpsa@gmail.com>
2020-09-11 14:10:35 -0300

    Hero additional info section created

--- src/pages/HeroDetails/index.tsx
+++ src/pages/HeroDetails/index.tsx
@@ -3,6 +3,11 @@ import { StatusBar } from 'expo-status-bar';
 import { useNavigation } from '@react-navigation/native';
 
 import Back from '../../assets/icons/Back.svg';
+import Age from '../../assets/icons/Age.svg';
+import Weight from '../../assets/icons/Weight.svg';
+import Height from '../../assets/icons/Height.svg';
+import Universe from '../../assets/icons/Universe.svg';
+
 import CoverSpiderMan from '../../assets/images/CoverSpiderManBig.png';
 
 import {
@@ -11,6 +16,13 @@ import {
   HeroHeader,
   BackButton,
   HeroBackground,
+  Info,
+  Civil,
+  Name,
+  AdditionalInfo,
+  InfoSection,
+  InfoText,
+  IconContainer,
 } from './styles';
 
 import Characters from '../../components/Characters';
@@ -28,12 +40,49 @@ const HeroDetails: React.FC = () => {
       <StatusBar style="light" translucent />
 
       <HeroBackground source={CoverSpiderMan}>
+        <HeroHeader>
+          <BackButton onPress={() => handleGoBack()}>
+            <Back width={24} height={24} />
+          </BackButton>
+        </HeroHeader>
         <Container>
-          <HeroHeader>
-            <BackButton onPress={() => handleGoBack()}>
-              <Back width={24} height={24} />
-            </BackButton>
-          </HeroHeader>
+          <Info>
+            <Civil>Peter Parker</Civil>
+            <Name>Homem Aranha</Name>
+          </Info>
+          <AdditionalInfo>
+            <InfoSection>
+              <IconContainer>
+                <Age width={20} height={20} />
+              </IconContainer>
+
+              <InfoText>30 anos</InfoText>
+            </InfoSection>
+
+            <InfoSection>
+              <IconContainer>
+                <Weight width={20} height={20} />
+              </IconContainer>
+
+              <InfoText>78kg</InfoText>
+            </InfoSection>
+
+            <InfoSection>
+              <IconContainer>
+                <Height width={20} height={20} />
+              </IconContainer>
+
+              <InfoText>1.80m</InfoText>
+            </InfoSection>
+
+            <InfoSection>
+              <IconContainer>
+                <Universe width={20} height={20} />
+              </IconContainer>
+
+              <InfoText>Terra 616</InfoText>
+            </InfoSection>
+          </AdditionalInfo>
 
           <Characters sectionName="Filmes" sectionData={movies} movieSection />
         </Container>
--- src/pages/HeroDetails/styles.ts
+++ src/pages/HeroDetails/styles.ts
@@ -19,8 +19,6 @@ export const Container = styled.View`
 export const HeroHeader = styled.View`
   flex-direction: row;
   max-height: 64px;
-  justify-content: space-between;
-  align-content: center;
 
   padding: 20px 24px;
 `;
@@ -30,4 +28,44 @@ export const BackButton = styled.TouchableOpacity``;
 export const HeroBackground = styled.ImageBackground`
   width: 100%;
   height: 100%;
+`;
+
+export const Info = styled.View`
+  margin-top: 230px;
+  max-width: 50%;
+`;
+
+export const Civil = styled.Text`
+  font-family: gilroy_light;
+  font-size: 16px;
+  color: ${colors.white};
+`;
+
+export const Name = styled.Text`
+  font-family: gilroy_extrabold;
+  font-size: 40px;
+  color: ${colors.white};
+`;
+
+export const AdditionalInfo = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  margin-top: 48px;
+  margin-bottom: 24px;
+`;
+
+export const IconContainer = styled.View`
+  justify-content: center;
+  align-items: center;
+`;
+
+export const InfoSection = styled.View`
+  justify-content: space-between;
+  height: 48px;
+`;
+
+export const InfoText = styled.Text`
+  font-family: gilroy_light;
+  font-size: 12px;
+  color: ${colors.white};
 `;
